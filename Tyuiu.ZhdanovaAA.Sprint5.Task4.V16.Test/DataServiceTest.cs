f4031d47b6eb41babef510f5a8e71886e5211a79
ZhdanovaAnna <annaone01@mail.ru>
2023-11-24 21:10:48 +0500

    Task4

--- Tyuiu.ZhdanovaAA.Sprint5.Task4.V16.Test/DataServiceTest.cs
+++ Tyuiu.ZhdanovaAA.Sprint5.Task4.V16.Test/DataServiceTest.cs
@@ -19,5 +19,16 @@ namespace Tyuiu.ZhdanovaAA.Sprint5.Task4.V16.Test
             bool wait = true;
             Assert.AreEqual(wait, fileExists);
         }
+
+        [TestMethod]
+        public void CheckLoadFromDataFile()
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\DataSprint5\InPutDataFileTask4V16.txt";
+            double res = ds.LoadFromDataFile(path);
+            double wait = 13.935;
+            Assert.AreEqual(wait, res);
+        }
     }
 }
